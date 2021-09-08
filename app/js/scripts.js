dd27ac4aa8f90777dc2d2bd2eb02060301e56f45
Daniil <danilsda78>
2021-09-08 20:38:00 +0300

    add funtion header scroll

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,5 +1,7 @@
 let mobMenu = document.querySelector('.mobile-menu')
 let mobBtn = document.querySelector('.header__btn')
+let header = document.querySelector('.header')
+let lastPositionScroll = 0
 
 
 function headBtnActive() {
@@ -7,4 +9,21 @@ function headBtnActive() {
 	mobBtn.classList.toggle('active')
 }
 
+function bodyPositionScroll() {
+	let i = window.scrollY
+	if (window.scrollY == 0) {
+		header.classList.remove('scroll')
+		header.classList.remove('defolt')
+	} else if (lastPositionScroll < i) {
+		header.classList.remove('scroll')
+		header.classList.add('defolt')
+	} else if (lastPositionScroll > i && window.scrollY !== 0) {
+		header.classList.remove('defolt')
+		header.classList.add('scroll')
+	}
+	lastPositionScroll = i
+}
+
+
 mobBtn.addEventListener('click', headBtnActive)
+window.addEventListener('scroll', bodyPositionScroll)
