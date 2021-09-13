f492fe9ce2963f5bc66397e609d9a10d6ef218d7
Daniil <danilsda78>
2021-09-13 11:01:45 +0300

    test

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -7,16 +7,18 @@ let cards = document.querySelectorAll('.pick__cards')
 let popap = document.querySelector('#popap')
 let popapOn = document.querySelector('.header__item.drop')
 let popapOff = document.querySelector('#popap .popap__btn')
+let scroll = true
 
 
 function headBtnActive() {
 	mobMenu.classList.toggle('active')
 	mobBtn.classList.toggle('active')
-	app.classList.toggle('hidden')
+	document.body.classList.toggle('hidden')
+	scroll = false
 }
 
 function bodyPositionScroll() {
-	if (popap.value) return
+	if (!scroll) return
 	let i = window.scrollY
 	if (window.scrollY == 0) {
 		header.classList.remove('scroll')
@@ -46,12 +48,12 @@ cards.forEach(card => {
 mobBtn.addEventListener('click', headBtnActive)
 window.addEventListener('scroll', bodyPositionScroll)
 popapOn.addEventListener('click', el => {
-	popap.value = true
+	scroll = true
 	popap.classList.toggle('active')
 	app.classList.toggle('hidden')
 })
 popapOff.addEventListener('click', el => {
-	popap.value = false
+	scroll = false
 	popap.classList.toggle('active')
 	app.classList.toggle('hidden')
 })
